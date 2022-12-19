c7e3352d764538d4d3504de4cd35bdd8dd6426e8
Pedro Passos <pedro.passos@hotmail.com>
2022-12-18 23:03:12 -0300

    Create a008.py

---
+++ aulas/a008.py
@@ -0,0 +1,11 @@
+from math import sqrt, floor    #importando especificamente funções da biblioteca math do python
+import random                   #importando a funcao random,
+
+#calculando a raiz quadrada de um número usandoa a biblioteca math com a funcao sqrt(x)
+# numero = int(input("Digite um número: "))
+# raiz_quadrada = sqrt(numero)
+# print("Raiz Quadrada do número {} é: {}.".format(numero, floor(raiz_quadrada)))
+
+#usando a random
+numero_2 = random.randint(1, 100)
+print(numero_2)
