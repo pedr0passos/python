83f3d66a96614f02da63e8f175dd62afe0c9fc52
Pedro Passos <pedro.passos@hotmail.com>
2022-12-21 17:15:59 -0300

    exercicios-e-aula09

---
+++ aulas/a009.py
@@ -0,0 +1,6 @@
+# aula de strings
+
+frase = 'Pedro é Lindo'
+
+# fatiamento de uma string
+print(frase[:13:2])
---
+++ exercicios/ex021.py
@@ -0,0 +1,8 @@
+# importanto o pygame
+import pygame
+
+# iniciando o pygame
+pygame.init()
+pygame.mixer.music.load('midia/musica.mp3')
+pygame.mixer.music.play()
+pygame.event.wait()
