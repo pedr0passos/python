8772774da41be7da965f8f4641273a4728120f45
Pedro Passos <102426718+pedr0passos@users.noreply.github.com>
2022-12-26 12:04:30 -0300

    aula09

--- aulas/a009.py
+++ aulas/a009.py
@@ -1,6 +1,17 @@
 # aula de strings
 
-frase = 'Pedro é Lindo'
+frase = '   Aprenda Python  '
 
 # fatiamento de uma string
-print(frase[:13:2])
+print(frase)
+print(frase.find('Android'))
+
+# Transformação
+frase2 = frase.replace('Python', 'Android')
+print(frase2)
+
+print(frase.upper())
+print(frase.title())
+frase3 = frase.strip()
+print(frase3)
+
